50a740a09e238ec22c17138cd9a659ba4b63d307
Salvador <salvadoredgar99@gmail.com>
2015-06-19 14:56:10 -0500

    New features

--- app/src/main/java/com/example/sati/ejemplocontaduria/MainActivity.java
+++ app/src/main/java/com/example/sati/ejemplocontaduria/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sati.ejemplocontaduria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,12 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button button;
     private TextView textView;
+
+    private Button buttonOtherActivity;
 
 
     @Override
@@ -25,12 +29,29 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textVIew);
 
+        buttonOtherActivity = (Button) findViewById(R.id.buttonOtherActivity);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textView.setText(editText.getText());
             }
         });
+
+        buttonOtherActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),OtherActivity.class);
+                intent.putExtra("ARG_TEXT", editText.getText().toString());
+
+                startActivity(intent);
+
+                Toast.makeText(getApplicationContext(), "Cambiaste de Actividad", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 }
